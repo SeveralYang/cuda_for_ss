62ea83b03023156ba10e876750fe6fb19f5b4b69
SeveralYang <96507091+SeveralYang@users.noreply.github.com>
2023-03-18 14:12:04 +0800

    Add files via upload

---
+++ test_ss_gpu02.py
@@ -0,0 +1,400 @@
+# -*- coding: utf-8 -*-
+import math
+import threading
+
+import skimage.io
+import skimage.feature
+import skimage.color
+import skimage.transform
+import skimage.util
+import skimage.segmentation
+import numpy
+import warnings
+import cv2
+import numba
+
+from time import time
+from numba import cuda, jit
+import selectivesearch.selectivesearch as ss
+
+warnings.filterwarnings('ignore')
+STATIC_DICT = {}
+
+
+# "Selective Search for Object Recognition" by J.R.R. Uijlings et al.
+#
+#  - Modified version with LBP extractor for texture vectorization
+
+def _generate_segments(im_orig, scale, sigma, min_size):
+    """
+        segment smallest regions by the algorithm of Felzenswalb and
+        Huttenlocher
+    """
+
+    # open the Image
+    im_mask = skimage.segmentation.felzenszwalb(
+        skimage.util.img_as_float(im_orig), scale=scale, sigma=sigma,
+        min_size=min_size)
+
+    # merge mask channel to the image as a 4th channel
+    im_orig = numpy.append(
+        im_orig, numpy.zeros(im_orig.shape[:2])[:, :, numpy.newaxis], axis=2)
+    im_orig[:, :, 3] = im_mask
+
+    return im_orig
+
+
+def _sim_colour(r1, r2):
+    """
+        calculate the sum of histogram intersection of colour
+    """
+    return sum([min(a, b) for a, b in zip(r1["hist_c"], r2["hist_c"])])
+
+
+def _sim_texture(r1, r2):
+    """
+        calculate the sum of histogram intersection of texture
+    """
+    return sum([min(a, b) for a, b in zip(r1["hist_t"], r2["hist_t"])])
+
+
+def _sim_size(r1, r2, imsize):
+    """
+        calculate the size similarity over the image
+    """
+    return 1.0 - (r1["size"] + r2["size"]) / imsize
+
+
+def _sim_fill(r1, r2, imsize):
+    """
+        calculate the fill similarity over the image
+    """
+    bbsize = (
+            (max(r1["max_x"], r2["max_x"]) - min(r1["min_x"], r2["min_x"]))
+            * (max(r1["max_y"], r2["max_y"]) - min(r1["min_y"], r2["min_y"]))
+    )
+    return 1.0 - (bbsize - r1["size"] - r2["size"]) / imsize
+
+
+def _calc_sim(r1, r2, imsize):
+    return (_sim_colour(r1, r2) + _sim_texture(r1, r2)
+            + _sim_size(r1, r2, imsize) + _sim_fill(r1, r2, imsize))
+
+
+def _calc_colour_hist(img):
+    """
+        calculate colour histogram for each region
+
+        the size of output histogram will be BINS * COLOUR_CHANNELS(3)
+
+        number of bins is 25 as same as [uijlings_ijcv2013_draft.pdf]
+
+        extract HSV
+    """
+
+    BINS = 25
+    hist = numpy.array([])
+
+    for colour_channel in (0, 1, 2):
+        # extracting one colour channel
+        c = img[:, colour_channel]
+
+        # calculate histogram for each colour and join to the result
+        hist = numpy.concatenate(
+            [hist] + [numpy.histogram(c, BINS, (0.0, 255.0))[0]])
+        # histogram(a,bins=10,range=None,weights=None,density=False);
+        # a是待统计数据的数组；
+        # bins指定统计的区间个数；
+        # range是一个长度为2的元组，表示统计范围的最小值和最大值，默认值None，表示范围由数据的范围决定
+        # weights为数组的每个元素指定了权值,histogram()会对区间中数组所对应的权值进行求和
+        # density为True时，返回每个区间的概率密度；为False，返回每个区间中元素的个数
+
+    # L1 normalize
+    hist = hist / len(img)
+
+    return hist
+
+
+def _calc_texture_gradient(img):
+    """
+        calculate texture gradient for entire image
+
+        The original SelectiveSearch algorithm proposed Gaussian derivative
+        for 8 orientations, but we use LBP instead.
+
+        output will be [height(*)][width(*)]
+    """
+    ret = numpy.zeros((img.shape[0], img.shape[1], img.shape[2]))
+
+    for colour_channel in (0, 1, 2):
+        ret[:, :, colour_channel] = skimage.feature.local_binary_pattern(
+            img[:, :, colour_channel], 8, 1.0)
+
+    return ret
+
+
+def _calc_texture_hist(img):
+    """
+        calculate texture histogram for each region
+
+        calculate the histogram of gradient for each colours
+        the size of output histogram will be
+            BINS * ORIENTATIONS * COLOUR_CHANNELS(3)
+    """
+    BINS = 10
+
+    hist = numpy.array([])
+
+    for colour_channel in (0, 1, 2):
+        # mask by the colour channel
+        fd = img[:, colour_channel]
+
+        # calculate histogram for each orientation and concatenate them all
+        # and join to the result
+        hist = numpy.concatenate(
+            [hist] + [numpy.histogram(fd, BINS, (0.0, 1.0))[0]])
+
+    # L1 Normalize
+    hist = hist / len(img)
+
+    return hist
+
+
+def _extract_regions(img):
+    # device_img = cuda.to_device(img)
+
+    R = {}
+
+    # get hsv image
+    hsv = skimage.color.rgb2hsv(img[:, :, :3])
+
+    # pass 1: count pixel positions
+    for y, i in enumerate(img):
+        for x, (r, g, b, l) in enumerate(i):
+            # initialize a new region
+            # YY 此处可以进行优化
+            if l not in R:
+                R[l] = {
+                    "min_x": 0xffff, "min_y": 0xffff,
+                    "max_x": 0, "max_y": 0, "labels": [l]
+                }
+            # bounding box
+            if R[l]["min_x"] > x:
+                R[l]["min_x"] = x
+            if R[l]["min_y"] > y:
+                R[l]["min_y"] = y
+            if R[l]["max_x"] < x:
+                R[l]["max_x"] = x
+            if R[l]["max_y"] < y:
+                R[l]["max_y"] = y
+
+    # pass 2: calculate texture gradient
+    tex_grad = _calc_texture_gradient(img)
+
+    # pass 3: calculate colour histogram of each region
+    for k, v in R.items():
+        # colour histogram
+        masked_pixels = hsv[:, :, :][img[:, :, 3] == k]
+        R[k]["size"] = len(masked_pixels / 4)
+        R[k]["hist_c"] = _calc_colour_hist(masked_pixels)
+        # texture histogram
+        R[k]["hist_t"] = _calc_texture_hist(tex_grad[:, :][img[:, :, 3] == k])
+
+    return R
+
+
+def _extract_neighbours(regions):
+    def intersect(a, b):
+        if (a["min_x"] < b["min_x"] < a["max_x"] and a["min_y"] < b["min_y"] < a["max_y"]) or (
+                a["min_x"] < b["max_x"] < a["max_x"] and a["min_y"] < b["max_y"] < a["max_y"]) or (
+                a["min_x"] < b["min_x"] < a["max_x"] and a["min_y"] < b["max_y"] < a["max_y"]) or (
+                a["min_x"] < b["max_x"] < a["max_x"] and a["min_y"] < b["min_y"] < a["max_y"]):
+            return True
+        return False
+
+    R = list(regions.items())
+    neighbours = []
+    for cur, a in enumerate(R[:-1]):
+        for b in R[cur + 1:]:
+            if intersect(a[1], b[1]):
+                neighbours.append((a, b))
+
+    return neighbours
+
+
+def _merge_regions(r1, r2):
+    new_size = r1["size"] + r2["size"]
+    rt = {
+        "min_x": min(r1["min_x"], r2["min_x"]),
+        "min_y": min(r1["min_y"], r2["min_y"]),
+        "max_x": max(r1["max_x"], r2["max_x"]),
+        "max_y": max(r1["max_y"], r2["max_y"]),
+        "size": new_size,
+        "hist_c": (
+                          r1["hist_c"] * r1["size"] + r2["hist_c"] * r2["size"]) / new_size,
+        "hist_t": (
+                          r1["hist_t"] * r1["size"] + r2["hist_t"] * r2["size"]) / new_size,
+        "labels": r1["labels"] + r2["labels"]
+    }
+    return rt
+
+
+@numba.cuda.jit
+def get_R(img, res):
+    i, j = cuda.grid(2)
+    # i == cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    # j == cuda.threadIdx.y + cuda.blockDim.y * cuda.blockIdx.y:
+    if i < img.shape[0] and j < img.shape[1]:
+        r, g, b, l = img[i][j]
+        l = int(l)
+        res[l][0] = min(res[l][0], j)
+        res[l][1] = min(res[l][1], i)
+        res[l][2] = max(res[l][2], j)
+        res[l][3] = max(res[l][3], i)
+
+
+def thread_task_convert(src, start_index, end_index):
+    for index in range(start_index, end_index, 10):
+        v = src[index, :]
+        if v[0] != 1080 or v[1] != 1920 or v[2] != 0 or v[3] != 0:
+            STATIC_DICT[float(index)] = {
+                'min_x': v[0],
+                'min_y': v[1],
+                'max_x': v[2],
+                'max_y': v[3],
+                'labels': [index]
+            }
+
+
+def thread_task_colour_histogram(k, img, hsv, tex_grad):
+    masked_pixels = hsv[:, :, :][img[:, :, 3] == k]
+    STATIC_DICT[k]["size"] = len(masked_pixels / 4)
+    STATIC_DICT[k]["hist_c"] = _calc_colour_hist(masked_pixels)
+    STATIC_DICT[k]["hist_t"] = _calc_texture_hist(tex_grad[:, :][img[:, :, 3] == k])
+    # print(STATIC_DICT[k])
+
+
+def selective_search_with_cuda(
+        im_orig, scale=1.0, sigma=0.8, min_size=50):
+    assert im_orig.shape[2] == 3, "3ch image is expected"
+
+    img = _generate_segments(im_orig, scale, sigma, min_size)
+    if img is None:
+        return None, {}
+    imsize = img.shape[0] * img.shape[1]
+    hsv = skimage.color.rgb2hsv(img[:, :, :3])
+    # ==========GPU加速==========
+    # 原_extract_regions()函数 pass1
+    tt0 = time()
+    R0 = numpy.zeros((300, 4))
+    R0[:, 0] = img.shape[1]
+    R0[:, 1] = img.shape[0]
+    device_img = cuda.to_device(img)
+    device_R = cuda.to_device(R0)
+    get_R[(100, 100, 1), (32, 32, 1)](device_img, device_R)
+
+    R0 = device_R.copy_to_host()
+    tt1 = time()
+    print(f"GPU time step1: {tt1 - tt0}")
+    # ==========GPU加速==========
+
+    # ==========step2==========
+    tex_grad = _calc_texture_gradient(img)
+    tt2 = time()
+    print(f"GPU time step2: {tt2 - tt1}")
+
+    # ==========数据转换=========
+    threads_tem = []
+    for i in range(10):  #
+        t = threading.Thread(target=thread_task_convert, args=(R0, i, 300))
+        threads_tem.append(t)
+    for t in threads_tem:
+        t.start()  # 循环 开始线程
+    for t in threads_tem:
+        t.join()
+    # ==========step3==========
+    count = 1
+    threads_tem = []
+    threads_total = []
+    for k, v in STATIC_DICT.items():  # 一共300个
+        count += 1
+        thread = threading.Thread(target=thread_task_colour_histogram, args=(k, img, hsv, tex_grad))
+        threads_tem.append(thread)
+        if count % 10 == 0:
+            # print(f"do {count}")
+            for t in threads_tem:
+                t.start()  # 循环 开始线程
+            threads_total += threads_tem
+            threads_tem.clear()
+    for t in threads_tem:
+        t.start()  # 循环 开始线程
+    for t in threads_total:
+        t.join()
+    tt3 = time()
+    print(f"GPU time step3 + Multithreading data convert time : {tt3 - tt2}")
+    while True:
+        if len(STATIC_DICT) == 300:
+            break
+    neighbours = _extract_neighbours(STATIC_DICT)
+    S = {}
+    for (ai, ar), (bi, br) in neighbours:
+        S[(ai, bi)] = _calc_sim(ar, br, imsize)
+    # hierarchal search
+    while S != {}:
+
+        # get highest similarity
+        i, j = sorted(S.items(), key=lambda i: i[1])[-1][0]
+
+        # merge corresponding regions
+        t = max(STATIC_DICT.keys()) + 1.0
+        STATIC_DICT[t] = _merge_regions(STATIC_DICT[i], STATIC_DICT[j])
+
+        # mark similarities for regions to be removed
+        key_to_delete = []
+        for k, v in S.items():
+            if (i in k) or (j in k):
+                key_to_delete.append(k)
+
+        # remove old similarities of related regions
+        for k in key_to_delete:
+            del S[k]
+
+        # calculate similarity set with the new region
+        for k in filter(lambda a: a != (i, j), key_to_delete):
+            n = k[1] if k[0] in (i, j) else k[0]
+            S[(t, n)] = _calc_sim(STATIC_DICT[t], STATIC_DICT[n], imsize)
+
+    regions = []
+    for k, r in STATIC_DICT.items():
+        regions.append({
+            'rect': (
+                r['min_x'], r['min_y'],
+                r['max_x'] - r['min_x'], r['max_y'] - r['min_y']),
+            'size': r['size'],
+            'labels': r['labels']
+        })
+
+    return img, regions
+
+
+def main(path="1.png"):
+    img = cv2.imread(path)
+    start = time()
+    img_lbl, regions = selective_search_with_cuda(img, scale=1000, sigma=0.8, min_size=50)
+    end = time()
+    print(f"total time {end - start}")
+    for r in regions:
+        x, y, h, w = r['rect']
+        x = int(x)
+        y = int(y)
+        h = int(h)
+        w = int(w)
+        cv2.rectangle(img, (x, y), (x + h, y + w), (0, 0, 255), 2)
+
+    cv2.imwrite("GPU_plus_res.jpg", img)
+    print("==========gpu_plus done==========")
+    print()
+
+
+if __name__ == '__main__':
+    main()
